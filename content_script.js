7694f2890bab0edf2652aeda171a4fb029004333
Wittakarn Keeratichayakorn <wittakarn.keeratichayakorn@bred-it.com>
2020-08-13 11:55:33 +0700

    Support multiple commands

--- content_script.js
+++ content_script.js
@@ -1,7 +1,8 @@
-chrome.storage.sync.get(["extensionRemoveElementSelector"], function (items) {
-  if (items["extensionRemoveElementSelector"]) {
-    const removeElement = document.querySelector(items["extensionRemoveElementSelector"]);
-    const parentOfTheRemoveElement = removeElement.parentNode;
-    parentOfTheRemoveElement.removeChild(removeElement);
+chrome.storage.sync.get(["extensionCommands"], function (items) {
+  if (items["extensionCommands"]) {
+    const commands = JSON.parse(items['extensionCommands']);
+    for (command of commands) {
+      document.querySelector(command.selector).setAttribute('style', command.styling);
+    }
   }
 });
--- popup.js
+++ popup.js
@@ -1,25 +1,55 @@
+const commandList = document.getElementById('command-list');
+const addCommandButton = document.getElementById('addCommand');
+const removeCommandButton = document.getElementById('removeCommand');
 const confirmButton = document.getElementById('confirm');
 
 window.onload = function () {
-    chrome.storage.sync.get(["extensionRemoveElementSelector"], function (items) {
-        if (items["extensionRemoveElementSelector"]) {
-            document.getElementById('removeElement').value = items["extensionRemoveElementSelector"];
+    chrome.storage.sync.get(['extensionCommands'], function (items) {
+        if (items['extensionCommands']) {
+            const commands = JSON.parse(items['extensionCommands']);
+
+            for(command of commands) {
+                commandList.appendChild(generateInput(command.selector, command.styling));
+            }
         }
     });
 }
 
-confirmButton.onclick = function (element) {
+confirmButton.onclick = function () {
+    const selectors = document.getElementsByName('selector');
+    const styles = document.getElementsByName('styling');
 
-    const elementSelector = document.getElementById('removeElement').value;
-    updateRemoveSelector(elementSelector);
+    const commandList = Array.prototype.map.call(selectors,
+        (selector, index) => (
+            {
+                selector: selector.value,
+                styling: styles[index].value
+            }
+        )
+    );
+    updateSelectorStyle(JSON.stringify(commandList));
 
     chrome.tabs.executeScript(null, {
         file: 'content_script.js'
     });
 };
 
-function updateRemoveSelector(value) {
-    chrome.storage.sync.set({ "extensionRemoveElementSelector": value }, function () {
+addCommandButton.onclick = function () {
+    commandList.appendChild(generateInput('', ''));
+};
+
+removeCommandButton.onclick = function () {
+    commandList.removeChild(commandList.lastChild);
+};
+
+function updateSelectorStyle(value) {
+    chrome.storage.sync.set({ 'extensionCommands': value }, function () {
         //  A data saved callback omg so fancy
     });
+}
+
+function generateInput(selector, styling) {
+    const row = document.createElement('tr');
+    row.innerHTML = '<tr><td><input type="text" name="selector" value="' + selector + '"/></td><td><input type="text" name="styling" value="' + styling + '"/></td></tr>';
+    return row;
 }
