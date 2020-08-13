5698b24719dc8938a9b7f38d326c6a734bd5fc05
Wittakarn Keeratichayakorn <wittakarn.keeratichayakorn@bred-it.com>
2020-08-13 14:08:08 +0700

    Support multiple element selector

--- content_script.js
+++ content_script.js
@@ -2,7 +2,14 @@ chrome.storage.sync.get(["extensionCommands"], function (items) {
   if (items["extensionCommands"]) {
     const commands = JSON.parse(items['extensionCommands']);
     for (command of commands) {
-      document.querySelector(command.selector).setAttribute('style', command.styling);
+      const elementSelectors = document.querySelectorAll(command.selector);
+      if (elementSelectors) {
+        for (elementSelector of elementSelectors) {
+          if (elementSelector) {
+            elementSelector.setAttribute('style', command.styling);
+          }
+        }
+      }
     }
   }
 });
